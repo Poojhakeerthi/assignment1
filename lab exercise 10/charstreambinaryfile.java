73eb51e4fcb1617be957b88e6ccab3b948b898ae
POOJHA KEERTHI S S <68847486+Poojhakeerthi@users.noreply.github.com>
2020-10-08 00:58:39 +0530

    Add files via upload

---
+++ lab exercise 10/charstreambinaryfile.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package labex10;
+import java.io.FileReader;
+import java.io.FileWriter;
+
+/**
+ *
+ * @author POOJHA KEERTHI
+ */
+public class charstreambinaryfile {
+    public static void main(String args[]) throws Exception {
+        FileReader fio=null;
+        FileWriter fou=null;
+        fio=new FileReader("C:\\Users\\POOJHA KEERTHI\\Pictures\\Downloaded pictures\\dp.jpeg");
+        fou=new FileWriter("dp_labex10_output.jpg");
+        int i;
+        while((i=fio.read())!=-1)
+        {
+            fou.write(i);
+        }
+        fou.close();
+    }
+}
